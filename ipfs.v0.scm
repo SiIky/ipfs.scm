5f89473debbb26707f175a4657fdc00f544d9237
siiky <siiky@users.noreply.github.com>
2022-02-16 23:07:45 +0000

    Add `writer/directory` & `writer/filesystem`
     * Implement `internal-writer`.
     * Add `writer/directory*` & `writer/file*`.
     * Reimplement `writer/file` in terms of `writer/file*`.

--- ipfs.v0.scm
+++ ipfs.v0.scm
@@ -13,8 +13,12 @@
 
    reader/json
    reader/plain
+   internal-writer
+   writer/directory
    writer/file
    writer/filesystem
+   writer/directory*
+   writer/file*
 
    Bool
    Int
@@ -33,16 +37,36 @@
             truncate
             write)
     (only chicken.base
+          alist-update
           assert
+          constantly
           cute
           define-constant
+          foldl
           make-parameter
-          o)
+          o
+          open-input-string
+          receive)
+    (only chicken.file
+          directory-exists?
+          file-exists?
+          find-files)
     (only chicken.io read-string)
     (only chicken.module export)
+    (only chicken.pathname
+          absolute-pathname?
+          decompose-directory
+          make-absolute-pathname
+          normalize-pathname)
+    (only chicken.process-context current-directory)
     (only chicken.string ->string))
 
   (import
+    ; NOTE: I'm not too comfortable with medea deserializing objects into
+    ;       alists with symbols as keys, but I assume no one will use this to
+    ;       communicate with a transmission instance they don't own, and thus
+    ;       possibly malicious...
+    ; TODO: Change the reader to use strings instead?
     (rename (only medea read-json)
             (read-json json:read))
     (rename (only uri-common make-uri)
@@ -56,10 +80,14 @@
   (import
     (only srfi-1
           append-map
-          every
-          filter)
+          filter
+          filter-map
+          last)
     (only srfi-13
-          string-join)
+          string-join
+          string-prefix-length
+          string-drop
+          string-trim-right)
     (only srfi-189
           just
           just?
@@ -98,22 +126,145 @@
   (define reader/json json:read)
   (define reader/plain read-string)
 
+  (define (internal-writer path #!optional name (headers '()))
+    `(file #:file ,path
+           ,@(if name `(#:filename ,name) '())
+           ,@(if (or (not headers) (null? headers))
+                 '()
+                 `(#:headers ,headers))))
+
+  (define (writer/file* path #!key name (headers '()))
+    (internal-writer path name headers))
+
   ;; @brief Compute the appropriate body for a single file.
-  ;; @param path The file's path.
-  ;; @param filename The filename sent in the request.
+  ;; @param path The file's path -- read by the client, not the IPFS node.
+  ;; @param name The name sent in the request; used as is.
   ;; @param headers Extra headers sent in the request for this file.
   ;; @returns An alist that can be used by `with-input-from-request` as the
   ;;   writer.
-  (define (writer/file path #!key filename headers)
-    (let ((filename (or filename path))
-          (filename-entry (if filename `(#:filename ,filename) '()))
-          (headers-entry (if headers `(#:headers ,headers) '())))
-      `((,filename #:file ,path ,@filename-entry ,@headers-entry))))
+  ;;
+  ;; If @a name is not given, `http-client` uses the basename of @a path.
+  (define (writer/file path #!key name (headers '()))
+    (list (writer/file* path #:name (or name (path->name path)) #:headers headers)))
 
-  ; TODO: Implement reading files & traversing a file system to write the
-  ;       request body.
-  (define (writer/filesystem path)
-    "")
+  (define (path-components path)
+    (receive (_ _ components) (decompose-directory path)
+      components))
+
+  (define (absolute-path-relative-to-directory base-dir-components)
+    (-> (normalize-pathname _)
+        (path-components _)
+        (make-absolute-pathname _ "")
+        (make-absolute-pathname base-dir-components _)
+        (normalize-pathname _)))
+
+  (define (longest-common-prefix strs)
+    (let ((s1 (car strs))
+          (strs (cdr strs)))
+      (foldl (lambda (l s2)
+               (let ((l (min l (string-length s2))))
+                 (string-prefix-length s1 s2 0 l 0 l)))
+             (string-length s1)
+             strs)))
+
+  ;; @brief Compute a suitable name for a file or directory given its path.
+  ;;
+  ;; Only useful for single files or directories, not lists of them, because it
+  ;; doesn't have enough context to compute a more meaninful (and unique)
+  ;; name.
+  (define (path->name path #!optional (cwd (current-directory)))
+    (let* ((cwd (path-components cwd)))
+      (=> path
+          ((absolute-path-relative-to-directory cwd) _)
+          (path-components _)
+          (last _))))
+
+  (define (writer/directory* path #!key name (headers '()))
+    (let ((empty-port (open-input-string ""))
+          (headers (alist-update 'content-type '(application/x-directory) headers)))
+      (internal-writer empty-port (or name path) headers)))
+
+  ;; @brief Compute the appropriate body for a single directory.
+  ;; @param path The directory's path -- read by the client, not the IPFS node.
+  ;; @param name The name sent in the request; used as is.
+  ;; @param headers Extra headers sent in the request for this directory.
+  ;; @returns An alist that can be used by `with-input-from-request` as the
+  ;;   writer.
+  ;;
+  ;; Always adds the Content-Type header set to application/x-directory. Sends
+  ;; the *directory only*, not the files inside it! For that @see
+  ;;   writer/filesystem.
+  ;;
+  ;; @see path->name
+  ;;
+  ;; TODO: Still not 100% satisfied with the behaviour...
+  (define (writer/directory path #!key name (headers '()))
+    (list (writer/directory* path #:name (or name (path->name path)) #:headers headers)))
+
+  ;; @brief Compute the appropriate body for the files of a filesystem tree.
+  ;; @returns An alist that can be used by `with-input-from-request` as the
+  ;;   writer.
+  ;;
+  ;; Wrapper around chicken.file.find-files. Except for `action` and `seed`
+  ;;   takes all the same arguments with the same semantics.
+  ;;
+  ;; @a path is read by the client, not the IPFS node.
+  ;;
+  ;; The resulting files and directories will be sent to the server with the
+  ;;   shortest possible names. That is, the longest common prefix of all the
+  ;;   paths found will be cut from all of the paths to form the name of each
+  ;;   file or directory entry.
+  ;;
+  ;; If @a path is relative, an attempt is made to transform it into an absolute path.
+  ;;
+  ;; @see chicken.file.find-files
+  (define (writer/filesystem path #!key test limit dotfiles follow-symlinks)
+    (assert (directory-exists? path) "path must be a directory")
+
+    (define cwd-components (path-components (current-directory)))
+    (define (abs-path-rel-to-cwd path)
+      (if (absolute-pathname? path)
+          path
+          ((absolute-path-relative-to-directory cwd-components) path)))
+
+    (define (predicate-mapper path)
+      (cond
+        ((directory-exists? path) path)
+        ((file-exists? path) (string-trim-right path #\/))
+        (else #f)))
+
+    (define (shorten-by-longest-prefix paths)
+      (let ((prefix-length (longest-common-prefix paths)))
+        (map (lambda (path)
+               (cons (string-drop path prefix-length) path))
+             paths)))
+
+    (define (writer shortened-path/full-path)
+      (let ((shortened-path (car shortened-path/full-path))
+            (full-path (cdr shortened-path/full-path)))
+        (cond
+          ((directory-exists? full-path)
+           (writer/directory* full-path #:name shortened-path))
+          ((file-exists? full-path)
+           (writer/file* full-path #:name shortened-path))
+          (else #f))))
+
+    ; TODO: Is it worth converting the resulting list of `find-files` to a
+    ;       stream to filter and map, and then convert back to a list? Should
+    ;       shorten the time between beginning to end of each file, thus
+    ;       lessening the probability of a TOCTOU error. It should make a
+    ;       significant difference for very large file trees.
+    (=> path
+        (abs-path-rel-to-cwd _)
+        (find-files
+          _
+          #:test (or test (constantly #t))
+          #:limit limit
+          #:dotfiles dotfiles
+          #:follow-symlinks follow-symlinks)
+        (filter-map predicate-mapper _)
+        (shorten-by-longest-prefix _)
+        (filter-map writer _)))
 
   ;; @see `uri-common`'s `make-uri`
   (define (make-uri #!key (scheme (*scheme*)) (host (*host*)) (port (*port*)) path query)
@@ -121,9 +272,7 @@
 
   ;; @see `intarweb`'s `make-request`
   (define (make-request uri)
-    (request:make
-      #:method 'POST
-      #:uri uri))
+    (request:make #:method 'POST #:uri uri))
 
   ;; @brief Wrapper around `http-client`'s `with-input-from-request`.
   ;; @see `http-client`'s `with-input-from-request`.
@@ -158,8 +307,8 @@
 
   (define (->maybe x)
     (if (maybe? x)
-      x
-      (just x)))
+        x
+        (just x)))
 
   (define (->list x) (if (list? x) x (list x)))
   (define ((list-wrapper type-cast) name value)
