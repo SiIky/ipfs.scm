bf33f8a7e9bf85c237cff3b1b321e25ae237f170
siiky <siiky@users.noreply.github.com>
2022-03-15 19:28:47 +0000

    Revert 3c305d41d013201a3e79219de7902d1a86773a43

--- ipfs.v0.scm
+++ ipfs.v0.scm
@@ -217,10 +217,10 @@
           (path-components _)
           (last _))))
 
-  (define (writer/directory* path #!key name (headers '()) avoid-chunking?)
+  (define (writer/directory* path #!key name (headers '()))
     (let ((name (or name (path->name path)))
           (headers (alist-update 'content-type '(application/x-directory) headers))
-          (file (and (not avoid-chunking?) (open-input-string ""))))
+          (file (open-input-string "")))
       (internal-writer file name headers)))
 
   ;; @brief Compute the appropriate body for a single directory.
@@ -237,8 +237,8 @@
   ;; @see path->name
   ;;
   ;; TODO: Still not 100% satisfied with the behaviour...
-  (define (writer/directory path #!key name (headers '()) avoid-chunking?)
-    (list (writer/directory* path #:name name #:headers headers #:avoid-chunking? avoid-chunking?)))
+  (define (writer/directory path #!key name (headers '()))
+    (list (writer/directory* path #:name name #:headers headers)))
 
   ;; @brief Compute the appropriate body for the files of a filesystem tree.
   ;; @returns An alist that can be used by `with-input-from-request` as the
@@ -257,7 +257,7 @@
   ;; If @a path is relative, an attempt is made to transform it into an absolute path.
   ;;
   ;; @see chicken.file.find-files
-  (define (writer/filesystem path #!key test limit dotfiles follow-symlinks avoid-chunking?)
+  (define (writer/filesystem path #!key test limit dotfiles follow-symlinks)
     (assert (directory-exists? path) "path must be a directory")
 
     (define cwd-components (path-components (current-directory)))
@@ -285,7 +285,7 @@
            ((directory-exists? full-path) writer/directory*)
            ((file-exists? full-path) writer/file*)
            (else (constantly #f)))
-         full-path #:name shortened-path #:avoid-chunking? avoid-chunking?)))
+         full-path #:name shortened-path)))
 
     (=> (find-files
           abs-path
